21e9056781dd308d87036c00d9da0fad2e0de774
Akhil Vudatha <akhilvsvs1999@gmail.com>
2024-02-26 01:33:02 +0530

    Initial Commit

---
+++ src/main/java/com/scrum/ScrumStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumStatisticsApplication.class, args);
+	}
+
+}
